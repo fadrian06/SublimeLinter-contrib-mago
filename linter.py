80e7fe6707a1e32f6be33c6be85afdb7ac0f5e47
fadrian06 <franyeradriansanchez@gmail.com>
2026-06-20 21:16:34 -0400

    Implement MagoFormat

--- linter.py
+++ linter.py
@@ -3,6 +3,7 @@ from typing import Iterable
 
 from SublimeLinter.lint.base_linter.php_linter import PhpLinter
 from SublimeLinter.lint.linter import LintMatch
+from sublime import FindFlags
 
 SELECTOR = "embedding.php"
 REPORTING_FORMAT = "github"
@@ -85,4 +86,55 @@ class MagoFormat(Mago):
     regex: "None | str | Pattern[str]" = None
 
     def find_errors(self, output: str) -> Iterable[LintMatch]:
-        return []
+        output_lines: list[str] = []
+        lint_matches: Iterable[LintMatch] = []
+
+        for output_line in output.splitlines():
+            if (
+                not output_line.startswith('-')
+                and not output_line.startswith('+')
+            ):
+                continue
+
+            if (
+                output_line.startswith('---')
+                or output_line.startswith('+++')
+            ):
+                continue
+
+            output_lines.append(output_line)
+
+        index = 0
+
+        while index < len(output_lines):
+            filtered_output_line = output_lines[index]
+
+            if filtered_output_line.startswith('-'):
+                region = self.view.find(
+                    filtered_output_line[1:],
+                    0,
+                    FindFlags.LITERAL,
+                )
+
+                line = self.view.rowcol(region.a)[0]
+                message = ''
+                next_index = index + 1
+
+                while next_index < len(output_lines):
+                    next_filtered_output_line = output_lines[next_index]
+
+                    if not next_filtered_output_line.startswith('+'):
+                        break
+
+                    message += next_filtered_output_line[1:] + '\n'
+                    next_index += 1
+
+                lint_matches.append(LintMatch({
+                    'line': line,
+                    'end_line': line,
+                    'message': message,
+                }))
+
+            index += 1
+
+        return lint_matches
